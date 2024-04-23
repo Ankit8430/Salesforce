b09f580fded3efe0ef41abce8c2223534de23d85
Ankit8430 <ankitgu8430@gmail.com>
2024-04-23 11:51:50 +0530

    Basic LWC

---
+++ force-app/main/default/lwc/loopingLWC/loopingLWC.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopingLWC extends LightningElement {
+    carList=["BMW","Audi","Fronx"]
+
+    ceoList=[
+        {
+            id:1,
+            company:"Mirketa",
+            Name:"Ankit Gupta"
+        },
+        {
+            id:2,
+            company:"Apple",
+            Name:"Naman"
+        }
+    ]
+}
